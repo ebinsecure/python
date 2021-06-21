2270fbf52d2104081abf6e60f837cc1ef6d893ea
ebinsecure <86216262+ebinsecure@users.noreply.github.com>
2021-06-21 09:03:07 +0530

    Add files via upload

---
+++ os.py
@@ -0,0 +1,68 @@
+import os
+print (os.ctermid)
+print (os.environ)
+print (os.environb)
+print (os.chdir)
+print (os.fchdir)
+print (os.getcwd)
+print (os.fsencode)
+print (os.fsdecode)
+print (os.fspath)
+print (os.pathlike)
+print (os.getenv)
+print (os.getenvb)
+print (os.get_exec_path)
+print (os.os.getegid())
+print (os.geteuid())
+print (os.getgid())
+print (os.getgrouplist())
+print (os.getgroups())
+print (os.getlogin())
+print (os.getpgid(pid))
+print (os.getpgrp())
+print (os.getpid())
+print (os.getppid())
+print (getpriority(which, who))
+print (os.getresuid())
+print (os.getresgid())
+print (os.getuid())
+print (os.initgroups(username, gid))
+print (os.putenv(key, value))
+print (os.setegid(egid))
+print (os.seteuid(euid))
+print (os.setgid(gid))
+print (os.setgroups(groups))
+print (os.setpgrp())
+print (os.setpgid(pid, pgrp))
+print (os.setpriority(which, who, priority))
+print (os.setregid(rgid, egid))
+print (os.setresgid(rgid, egid, sgid))
+print (os.setresuid(ruid, euid, suid))
+print (setreuid(ruid, euid))
+print (os.getsid(pid))
+print (os.setsid())
+print (os.setuid(uid))
+print (os.strerror(code))
+print (os.umask(mask))
+print (os.uname())
+print (os.unsetenv(key))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
